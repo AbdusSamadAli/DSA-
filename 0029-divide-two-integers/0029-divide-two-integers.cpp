fc4444639fb473faf2788977840709d467f02222
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-02-28 10:37:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         if(dividend == INT_MIN && divisor==-1) return INT_MAX;
-         if(dividend == INT_MIN && divisor==1) return INT_MIN;
+        if(dividend == INT_MIN && divisor==1) return INT_MIN;
         bool isNegative = (divisor<0) ^ (dividend<0);
         long long num = (long long)dividend;
         long long den = (long long)divisor;
-        num = llabs(num);
-        den = llabs(den);
         int quotient=0;
         while(num>=den){
             long long temp = den, multiple=1;
