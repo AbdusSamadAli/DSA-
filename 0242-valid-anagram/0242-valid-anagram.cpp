1b549d4866c358b25ac0ac3faf1511aef79a64d4
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-03-09 22:40:26 +0530

    Time: 7 ms (22.41%), Space: 9.7 MB (48.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()){
-            return false;
-        }
-        unordered_map<char,int>m;
-        for(char c:s){
-            m[c]++;
-        }
-        for(char c:t){
-            if(m.find(c)== m.end() || m[c]==0){
-                return false;
-            }
-            m[c]--;
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return t==s;
     }
 };
