47349e1aaccf9dc07c90a57843ec50d0c7c77ff1
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-03-14 23:43:01 +0530

    Time: 0 ms (100%), Space: 52.2 MB (86.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,10 +2,17 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        int goal = n-1;
-        for(int i= n-1; i>=0;i--){
-            if(i+nums[i] >= goal) goal = i;
+        int goal = 0; // Tracks the farthest reachable index
+        for(int i= 0; i<n;i++){
+            if(i>goal) return false; // If i is beyond our reachable range, return false
+            goal = max(goal, i+nums[i]); // Update the farthest we can gp
         }
-        return goal == 0;
+        return true; // If we reach here, we can reach the last index
     }
 };
+
+//greedy approach
+//1️⃣ Solve Jump Game II (Min Jumps to End) to reinforce this logic.
+//2️⃣ Solve Can Place Flowers for another easy Greedy problem.
+// 3)  Gas Station (Circular Jumping, Similar Concept)
+// 4) Farthest Building You Can Reach (Greedy Heap Approach)
