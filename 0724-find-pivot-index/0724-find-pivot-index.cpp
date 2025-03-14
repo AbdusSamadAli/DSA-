95d9ef9d9c8a343a1a8485781e5bab497075b646
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-03-14 18:03:19 +0530

    Time: 0 ms (100%), Space: 35.7 MB (77.29%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int totalsum=0, leftsum=0;
-        for(int n:nums) totalsum = totalsum + n;
+        for(int n:nums) totalsum = totalsum + n; //compute total sum
         for(int i=0;i<nums.size();i++){
             if(leftsum == totalsum - leftsum - nums[i])
                  return i; //checks if the left sum is equal to the right sum at index i.
-            leftsum = leftsum + nums[i];
+            leftsum = leftsum + nums[i]; //update leftsum
         }
-        return -1;
+        return -1; //return -1 if pivot index not found
     }
 };
 //Prefix sum concept
