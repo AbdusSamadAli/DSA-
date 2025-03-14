88cf9e73b288d474638ea10adea91438991c4701
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-03-14 23:26:03 +0530

    Time: 3 ms (28.08%), Space: 52.4 MB (31.51%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,24 +2,10 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<bool>dp(n,false);  // Base case: Last index is always reachable
-        dp[n-1] = true;
-        for(int i= n-2; i>=0; i--){ 
-            int maxjump = min(nums[i]+i, n-1);  // Find the farthest we can jump
-            for(int j=i+1; j<=maxjump; j++){
-                if(dp[j]){
-                    dp[i] = true; 
-                    break;
-                } 
-            }
+        int goal = n-1;
+        for(int i= n-1; i>=0;i--){
+            if(i+nums[i] >= goal) goal = i;
         }
-        return dp[0];
+        return goal == 0;
     }
 };
-
-
-
-//Pattern Name: "Reachability DP" + "Bottom-Up DP"
-//"Can you reach a destination?" → Think DP or BFS.
-//"Work backwards" → Try bottom-up DP.
-//"Can I make the next move using previous decisions?" → Greedy Optimization.
