8642b769e311cea6757943ffb56ff6dce4d64ac2
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-07-12 00:38:38 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.7%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -3,8 +3,8 @@ public:
     int kthGrammar(int n, int k) {
         if(n==1) return 0;
         int mid = pow(2,n-1) / 2;
-        if(k<=mid) return kthGrammar(n-1,k);
-        else return 1 - kthGrammar(n-1, k-mid);
+        if(k<=mid) return kthGrammar(n-1,k); //left half is same as previous row i.e n-1
+        else return 1 - kthGrammar(n-1, k-mid); //right half is flip of previous row so 1 - fn
     }
 };
 //Length of row = 2^n-1, so mid length = 2^n-1/2 i.e pow(2,n-1)/2
