7adcc3973de59d1e51c1412125d53fec78dc53bb
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-07-20 13:59:39 +0530

    Time: 0 ms (100%), Space: 9.9 MB (67.42%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,27 +1,25 @@
-#include <unordered_map>
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.size()>s2.size()){
-             return false;
+        if(s1.size()>s2.size()) return false;
+        vector<int>s1count(26,0);
+        vector<int>s2count(26,0);
+        for(int i=0; i<s1.size();i++){
+            s1count[s1[i] -'a']++;
+            s2count[s2[i] -'a']++;
         }
-        std::unordered_map<char,int>count;
-        for(char c:s1){
-            count[c]++;
-
+        int matches = 0;
+        for(int i=0; i<26;i++) matches = matches + (s1count[i]==s2count[i]);
+        for(int right=s1.size(), left=0; right<s2.size();right++,left++ ){
+            if(matches == 26) return true;
+            int index1 = s2[right] - 'a', index2 = s2[left] - 'a';
+            s2count[index1]++;
+            if(s1count[index1] == s2count[index1]) matches++;
+            else if(s2count[index1]-1 == s1count[index1]) matches--;
+            s2count[index2]--;
+            if(s1count[index2] == s2count[index2]) matches++;
+            else if(s2count[index2]+1 == s1count[index2]) matches--;
         }
-        int l=0,r=0,req=s1.size();
-        while(r<s2.size()){
-            if(count[s2[r++]]-- >0){
-                req--;
-            }
-            if(req==0){
-                return true;
-            }
-            if(r-l==s1.size()&& count[s2[l++]]++>=0){
-                req++;
-            }
-        }
-        return false;
+        return matches == 26;
     }
 };
