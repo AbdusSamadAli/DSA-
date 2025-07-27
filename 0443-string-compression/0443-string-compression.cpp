e7c9a4c63ace7d400653e6da35a36624f437e5dd
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2025-07-27 13:18:13 +0530

    Time: 0 ms (100%), Space: 13.8 MB (48.52%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -6,9 +6,8 @@ public:
         for(int i=0;i<n;i++){
             char ch = chars[i];
             int count = 0;
-            while(i<n && chars[i] == ch){
-                count++;
-                i++;
+            while(i<n && ch == chars[i]){
+                count++; i++;
             }
             if(count == 1) chars[index++] = ch;
             else{
@@ -18,7 +17,7 @@ public:
             }
             i--;
         }
-        chars.resize(index);
+        
         return index;
     }
 };
